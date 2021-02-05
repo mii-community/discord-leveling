1f43089243724a8e024e3280a29d6dacc3aeea81
tenzyu <tenzyu.on@gmail.com>
2021-02-05 09:27:35 +0900

    use Intents

--- launcher.py
+++ launcher.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from traceback import print_exc
 
+from discord import Intents
 from discord.ext.commands import Bot, when_mentioned_or
 
 import const
@@ -8,7 +9,9 @@ import const
 
 class MyBot(Bot):
     def __init__(self):
-        super().__init__(command_prefix=when_mentioned_or(const.BOT_PREFIX))
+        super().__init__(
+            command_prefix=when_mentioned_or(const.BOT_PREFIX), intents=Intents.all()
+        )
         print(f"Launching {const.BOT_NAME}")
 
         for cog in Path("cogs/").glob("*.py"):
